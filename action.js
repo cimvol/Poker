94f764222be89d74ff5be49f2f22a8b799af7c5c
Slav <cimvol@internet.ru>
2023-05-21 17:58:05 +0400

    Добавил правильные шрифты и указал верный путь

--- action.js
+++ action.js
@@ -1,3 +1,10 @@
-
-
-
+// нашел кнопку сложности 1
+const levelElement1 = document.getElementById("level1");
+// нахожу элемент цифры, который хочу подстветить при нажатии
+const namberLevelElement = document.getElementById("namberLevel");
+// оживляю кнопку, навешиваю на нее обработчик события клика
+levelElement1.addEventListener('click', () => {
+namberLevelElement.innerHTML = "easy";
+localStorage.setItem('key', 'easy');
+});
+window.localStorage.getItem('key');
