457901c596c7aac3891969d8250acb95e66203cf
Slav <cimvol@internet.ru>
2023-05-23 22:10:11 +0400

    Доработал код выбора сложности

--- action.js
+++ action.js
@@ -1,56 +1,47 @@
-let LS = localStorage;
 
-// нашел в интерфейсе кнопку сложности 1
 const levelEasyElement = document.getElementById("levelEasy");
-// нахожу цифру, которую хочу изменять при нажатии
 const namberEasyElement = document.getElementById("namberEasy");
-// оживляю кнопку, навешиваю на нее обработчик события клика
+const levelMeanElement = document.getElementById("levelMean");
+const namberMeanElement = document.getElementById("namberMean");
+const levelHardElement = document.getElementById("levelHard");
+const namberHardElement = document.getElementById("namberHard");
+
+
 levelEasyElement.addEventListener('click', () => {
     // записываю новое значение 
     namberEasyElement.innerHTML = 'easy';
-// сохряняю это значение в LS
- LS.setItem('nam1', JSON.stringify('easy'));
-});
+   // сохряняю это значение в LS
+   localStorage.setItem('nam1', 'easy');
+  });
 
-// нашел в интерфейсе кнопку сложности 2
-const levelMeanElement = document.getElementById("levelMean");
-// нахожу цифру, которую хочу изменить при нажатии
-const namberMeanElement = document.getElementById("namberMean");
-// оживляю кнопку, навешиваю на нее обработчик клика
 levelMeanElement.addEventListener('click', () => {
      // записываю новое значение 
     namberMeanElement.innerHTML = 'mean';
     // сохряняю это значение в LS
-    LS.setItem('nam2', JSON.stringify('mean'));
+    localStorage.setItem('nam2', 'mean');
    });
 
-// нашел в интерфейсе кнопку сложности 3
-const levelHardElement = document.getElementById("levelHard");
-// нахожу цифру, которую хочу изменить при нажатии
-const namberHardElement = document.getElementById("namberHard");
-// оживляю кнопку, навешиваю на нее обработчик клика
 levelHardElement.addEventListener('click', () => {
       // записываю новое значение 
     namberHardElement.innerHTML = 'hard';
     // сохряняю это значение в LS
-    LS.setItem('nam3', JSON.stringify('hard'));
+    localStorage.setItem('nam3', 'hard');
    });
 
-
    // провожу проверку для определения какой уровень выбран
-if (LS.getItem('nam1')) {
-    namberEasyElement.innerHTML = LS.getItem('nam1'); 
-    LS.removeItem('nam2');
-    LS.removeItem('nam3');
+if (localStorage.getItem('nam1')) {
+    localStorage.removeItem('nam3');
+    let first = namberEasyElement.innerHTML;
+    namberEasyElement.innerHTML = localStorage.getItem('nam1');  
 }
-if (LS.getItem('nam2')) {
-    LS.removeItem('nam1');
-    namberMeanElement.innerHTML = LS.getItem('nam2'); 
-    LS.removeItem('nam3');
- 
+if (localStorage.getItem('nam2')) {
+    localStorage.removeItem('nam1');
+    let second = namberMeanElement.innerHTML;
+    namberMeanElement.innerHTML = localStorage.getItem('nam2');
 }
-if (LS.getItem('nam3')) {
-    LS.removeItem('nam1');
-    LS.removeItem('nam2');
-    namberHardElement.innerHTML = LS.getItem('nam3'); 
+if (localStorage.getItem('nam3')) {
+    localStorage.removeItem('nam1');
+    localStorage.removeItem('nam2');
+    let third = namberHardElement.innerHTML;
+    namberHardElement.innerHTML = localStorage.getItem('nam3'); 
 }
